bd26a2475d843e0e7ad887a29141d8c69a459175
Antoine Rousselle <antoine.rousselle@epietch.eu>
2024-03-24 14:56:01 +0100

    [+] add systeme logo and put picture into web site

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,9 @@ import { ArrowBigUp, ArrowBigLeft } from "lucide-react";
 import { useState } from 'react';
 import MinecraftSkin from "./MinecraftSkin";
 import { saveVote } from "./InteractData";
+import facebookLogo from './facebook-logo.png';
+import telegramLogo from './telegram-logo.png';
+import TwitterLogo from './twitter-logo.jpg';
 
 function App() {
   const [username, setUsername] = useState('MartinDev');
@@ -25,7 +28,23 @@ function App() {
     saveVote(username, new Date().toDateString());
     event.target.innerHTML = 'Vote added!';
     event.target.disabled = true;
-  }
+  };
+  const shareOnFacebook = () => {
+    const quoteText = "Check out this awesome content on Example.com!";
+    const facebookShareUrl = `https://www.facebook.com/sharer/sharer.php?u=https://example.com&quote=${encodeURIComponent(quoteText)}`;
+    window.open(facebookShareUrl, "_blank");
+  };
+  const shareOnTelegram = () => {
+    const telegramShareUrl = "https://t.me/share/url?url=https://example.com&text=Check%20out%20this%20awesome%20content!";
+    window.open(telegramShareUrl, "_blank");
+  };
+  const shareOnTwitter = () => {
+    const tweetText = "Check out this awesome content!";
+    const tweetUrl = "https://my_memMC.com";
+    const twitterShareUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(tweetText)}&url=${encodeURIComponent(tweetUrl)}`;
+    window.open(twitterShareUrl, "_blank");
+  };
+
   return (
     <div className="h-screen w-screen flex flex-col bg-gray-200">
       <div className="w-full bg-black py-2">
@@ -85,6 +104,17 @@ function App() {
             </>
             }
           </Card>
+          <div className="mt-6 flex justify-center space-x-4">
+            <Button type="button" onClick={shareOnFacebook} className="bg-transparent border-none p-0 hover:bg-transparent hover:border-none">
+              <img src={facebookLogo} alt="Share on Facebook" className="h-8 w-8"/>
+            </Button>
+            <Button type="button" onClick={shareOnTelegram} className="bg-transparent border-none p-0 hover:bg-transparent hover:border-none">
+              <img src={telegramLogo} alt="Share on Telegram" className="h-8 w-8"/>
+            </Button>
+            <Button type="button" onClick={shareOnTwitter} className="bg-transparent border-none p-0 hover:bg-transparent hover:border-none">
+              <img src={TwitterLogo} alt="Share on Twitter" className="h-8 w-8"/>
+            </Button>
+          </div>
         </div>
       </div>
     </div>
